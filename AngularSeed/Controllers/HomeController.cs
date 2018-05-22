e8165b49df9a5a9acb59682eaf1582c7c7cfc4f2
Yashwanth Roy <Yashwanth.c@keka.com>
2018-05-22 15:31:07 +0530

    created angular seed project

---
+++ AngularSeed/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace AngularSeed.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
---
+++ AngularSeed/gulpfile.js
@@ -0,0 +1,14 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var uglify = require('gulp-minify');
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
